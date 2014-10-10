f9fafb56a020395844f795685d19d636b41b537d
Roman Platonov <rom16rus@gmail.com>
2014-10-11 00:02:12 +0400

    update 10.10

--- twitter/models.py
+++ twitter/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class RawTweet(models.Model):
+    text = models.CharField(max_length=140)
+    p_date = models.DateTimeField()
+
+#syncdb v dgango 1.7
--- twitter/views.py
+++ twitter/views.py
@@ -1,16 +1,20 @@
+from datetime import datetime
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 # Create your views here.
+from django.core.urlresolvers import reverse
+from twitter.models import RawTweet
+
 
 def index(request):
     return render(request, "twitter/index.html",
-                  {"user": "Huilo", "tweets": []
-                  }
+                  {"user": "Huilo", "tweets": RawTweet.objects.all().order_by("-p_date")}
     )
 
 
 def process(request):
     tweet = request.POST["tweet"]
-    print tweet
-    return HttpResponseRedirect("/")
+    t = RawTweet(text=tweet, p_date=datetime.now())
+    t.save()
+    return HttpResponseRedirect(reverse('index'))  # args=(book.id,)
